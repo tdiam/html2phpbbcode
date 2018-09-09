6e8bfebd25c9068bcd90d47c26a9d2432fd25fe4
Theo Diamantidis <diamaltho@gmail.com>
2018-09-09 22:40:00 +0300

    Added README content and dependencies in setup.py; Updated package url

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
+import os
 import sys
 
 from setuptools import setup
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(BASE_DIR, "README.md"), encoding="utf-8") as fp:
+    long_description = fp.read()
 
 setup(
     name="html2phpbbcode",
     version="0.1.0",
     packages=["html2phpbbcode"],
     license="BSD",
-    url="https://lib.ece.ntua.gr/git/tdiam/html2phpbbcode",
+    url="https://github.com/tdiam/html2phpbbcode",
     author="Theodoros Diamantidis",
     author_email="diamaltho@gmail.com",
-    description="HTML to phpBB BBCode converter",
+    description="HTML to phpBB-compatible BBCode converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_data={"html2phpbbcode": ["data/defaults.conf"]},
+    install_requires=[
+        "html2bbcode==2.3.3",
+        "regex==2018.8.29",
+    ],
     classifiers=[
         "Topic :: Utilities",
         "Topic :: Text Processing :: Markup :: HTML",
